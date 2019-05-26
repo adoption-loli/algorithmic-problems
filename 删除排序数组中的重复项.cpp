91ede19363805cf9f31d6ea564ad8dd18f3b0945
adoption <1509636344@qq.com>
2019-05-26 23:40:11 +0800

    第一遍.3

--- 删除排序数组中的重复项.cpp
+++ 删除排序数组中的重复项.cpp
@@ -1,10 +1,15 @@
+
+
 int removeDuplicates(int* nums, int numsSize){
         int i = 0, j = 0;
         if(numsSize == 0) return 0;
         for(;j < numsSize;j++)
         {
-            if(nums[i] != nums[j])
-                nums[++i] = nums[j];
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
         }
         return i+1;
 }
+
